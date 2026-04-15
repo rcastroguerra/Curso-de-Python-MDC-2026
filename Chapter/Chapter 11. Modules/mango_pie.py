04dc8914821f469143712451fda8f7d9181d0ece
rcastroguerra <raydel.castroguerra@gmail.com>
2026-04-15 10:13:32 -0400

    Subiendo todo el contenido inicial del curso

---
+++ Chapter/Chapter 11. Modules/mango_pie.py
@@ -0,0 +1,3 @@
+def cook(mango_quantity1, mango_quantity2):
+	print(f"{mango_quantity1} mangoes become one mango pie.")
+	print(f"{mango_quantity2} mangoes are used to decorate one mango pie.")
--- Chapter/Chapter 11. Modules/pie_shop.py
+++ Chapter/Chapter 11. Modules/pie_shop.py
@@ -1,2 +1,5 @@
 def order_pumpkin_pie(shopper_name, num_pumpkins):
 	print(f"{shopper_name} orders a pie with {num_pumpkins} pumpkins.")
+
+def order_mango_pie(buyer_name, mango_quantity):
+	print(f"{buyer_name} orders a pie with {mango_quantity} mangoes.")
